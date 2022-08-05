737bcdf62f10770dd9d1fa92bd95aad347b1ed10
zbl14 <ifthereisoneday@gmail.com>
2022-08-05 10:52:24 -0700

    add Index() with searching, Create()

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -3,12 +3,51 @@ using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
-// using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace Factory.Controllers
 {
   public class EngineersController : Controller
   {
+    private readonly FactoryContext _db;
 
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index(string searchString)
+    {
+      ViewBag.PageTitle = "View All Courses";
+      if (!String.IsNullOrEmpty(searchString))
+      {
+        List<Engineer> model = _db.Engineers
+          .Where(Engineer => Engineer.Name.Contains(searchString))
+          .OrderBy(Engineer => Engineer.Name)
+          .ToList();
+        return View(model);
+      }
+      else
+      {
+        List<Engineer> model = _db.Engineers
+          .OrderBy(Engineer => Engineer.Name)
+          .ToList();
+        return View(model);
+      }
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "New Engineer";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
